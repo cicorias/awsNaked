ca691d0f65e28662475905f57fe31e2ce4cca3fd
Shawn Cicoria <shawn@cicoria.com>
2014-08-26 16:59:44 -0400

    updates for unit test

--- NakedAws/RefreshRt53.Tests/UnitTest1.cs
+++ NakedAws/RefreshRt53.Tests/UnitTest1.cs
@@ -6,12 +6,18 @@ namespace RefreshRt53.Tests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        public TestContext TestContext { get; set; }
+
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\data.user","data#csv", 
+            DataAccessMethod.Sequential), DeploymentItem("data.user"), TestMethod]
         public void TestMethod1()
         {
             var mut = new RefreshRt53.UpdateRt53();
 
-            UpdateRt53.CheckAndSet("cloudexpert.io", "192.0.0.1", "cloudexpertio.wordpress.com", "Z39DT71SBE1W1D");
+            var hostName = (string)TestContext.DataRow["source"];
+
+            UpdateRt53.CheckAndSet(//);
 
 
         }
